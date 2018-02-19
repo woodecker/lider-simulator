b64fd964465dd271796e79fa6a754f63dcaaf742
Pedro Costa <woodecker@gmail.com>
2018-02-19 02:17:40 -0300

    Painel Relatorio

--- Lider Sim/Assets/Scripts/Projeto.cs
+++ Lider Sim/Assets/Scripts/Projeto.cs
@@ -8,27 +8,81 @@ public class Projeto : MonoBehaviour {
 	public static Projeto Instance;
 
 	public int orcamento = 50000;
+	public int orcamentoInicial;
 	public Text orcamentoText;
-	public Text lucrosText;
 
 	public List<Servico> currentServicos;
 	public List<Perfil> Equipe;
 
-	void Awake(){
+	public int organizacao;
+	public int programacao;
+	public int design;
+	public int criatividade;
+
+	public int dias;
+	public int energy;
+
+	public Text etapa;
+	public Slider timer;
+
+	void Awake ()
+	{
 		if (Instance == null)
 			Instance = this;
 	}
 
-	public void Contratar(int salario){
+	void Start ()
+	{
+		orcamentoInicial = orcamento;
+
+		orcamentoText.text = "R$ " + orcamento.ToString ();
+
+		timer.maxValue = 30;
+		timer.value = dias;
+	}
+
+	void Update ()
+	{
+		orcamentoText.text = "R$ " + orcamento.ToString ();
+
+		if (timer.value <= 1)
+			etapa.text = "PRE-PRODUÇÃO";
+		else if (timer.value <= 29)
+			etapa.text = "PRODUÇÃO";
+		else
+			etapa.text = "POS-PRODUÇÃO";
+	}
+
+	public void StartProject ()
+	{
+		InvokeRepeating ("NewDay", 0f, 30f);
+	}
+
+	public void NewDay ()
+	{
+		dias++;
+		timer.value = dias;
+
+		organizacao++;
+		programacao++;
+		design++;
+		criatividade++;
+		energy++;
+	}
+
+	public void Contratar (int salario)
+	{
 		orcamento -= salario;
 	}
-
-	void Start(){
-		orcamentoText.text = "R$ " + orcamento.ToString ();
+		
+	public int PontosProjeto ()
+	{
+		return (organizacao + programacao + design + criatividade) / 4;
 	}
 
-	void Update(){
-		orcamentoText.text = "R$ " + orcamento.ToString ();
-		//lucrosText.text = "R$ " + orcamento.ToString ();
+	public int Produtividade()
+	{
+		return energy;
 	}
+
 }
