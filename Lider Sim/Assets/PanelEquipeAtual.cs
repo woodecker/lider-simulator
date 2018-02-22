bcfe775e73ba612d739057c86a5309bb44bc0ef9
Pedro Costa <woodecker@gmail.com>
2018-02-22 04:54:44 -0300

    Minigames

---
+++ Lider Sim/Assets/PanelEquipeAtual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelEquipeAtual : MonoBehaviour {
+
+	public GameObject prefabPerfil;
+	public RectTransform parentPerfil;
+
+	public List<GameObject> myPerfis;
+
+	public void Start () 
+	{
+		//Create services
+		foreach (Perfil s in Projeto.Instance.Equipe) 
+		{
+			GameObject newButton = Instantiate (prefabPerfil, parentPerfil);
+			newButton.GetComponent<CardPerfil> ().SetPerfil (s);
+		}
+	}
+}
