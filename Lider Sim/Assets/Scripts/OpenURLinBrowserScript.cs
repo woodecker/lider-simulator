b79a777b7be5f1c5e4d9d9f65bb156ee57212423
Mauricio <mds694@nyu.com>
2018-03-14 15:13:04 -0300

    Adicionei um monte de painéis para o codex, faltam os botões que irão ativa-los.

---
+++ Lider Sim/Assets/Scripts/OpenURLinBrowserScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURLinBrowserScript : MonoBehaviour {
+
+	public string url;
+
+	public void OpenUrl(){
+		Application.OpenURL(url);
+	}
+}
