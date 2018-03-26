2bb1fd71f8e6f67bbab9547a5dee98fc484c14c6
Mauricio <mds694@nyu.com>
2018-03-26 15:49:42 -0300

    Tela que pausa quando o app sai de foco.

---
+++ Lider Sim/Assets/Scripts/CheckForAppPausedScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForAppPausedScript : MonoBehaviour {
+
+	bool isPaused = false;
+	public GameObject PauseScreen;
+
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			PauseScreen.SetActive (true);
+		} else {
+			PauseScreen.SetActive (false);
+		}
+	}
+	void OnApplicationFocus (bool hasFocus){
+		isPaused = !hasFocus;
+	}
+	void OnApplicationPause (bool pauseStatus){
+		isPaused = pauseStatus;
+	}
+}
