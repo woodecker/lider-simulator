387785eed941a6c6210da9db3ec008f7f6c4fc24
Mauricio <mds694@nyu.com>
2018-04-25 17:00:39 -0300

    Adicionei música, controle da mesma, e fiz uns ajustes finais.

---
+++ Lider Sim/Assets/Scripts/ChangeVolumeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class ChangeVolumeScript : MonoBehaviour {
+
+	public Slider volumeSlider;
+	public AudioMixer mixer;
+
+	void Start (){
+		mixer.SetFloat ("Volume", volumeSlider.value);
+	}
+
+	public void VolumeController(float volume){
+		mixer.SetFloat ("Volume", volume);
+	}
+}
